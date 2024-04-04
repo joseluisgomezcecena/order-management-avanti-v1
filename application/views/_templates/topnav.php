7cdb37ee8f4d2af637b035282ce6e741b3b18cdc
joseluis gomez <joseluisgomezcecegna@gmail.com>
2024-04-04 01:49:58 -0700

    Added letter avatars.

--- application/views/_templates/topnav.php
+++ application/views/_templates/topnav.php
@@ -95,18 +95,21 @@
                                 </div>
                             </div>
                         </li>
+
                         <li class="dropdown dropdown-animated scale-left">
                             <div class="pointer" data-toggle="dropdown">
-                                <div class="avatar avatar-image  m-h-10 m-r-15">
-                                    <img src="assets/images/avatars/thumb-3.jpg"  alt="">
+                                <div style="background-color:orange;" class="avatar avatar-image m-h-10 m-r-15">
+                                   <?php echo strtoupper(substr($this->session->userdata('username'), 0, 1)); //echo just the first letter of the username.?>
                                 </div>
                             </div>
                             <div class="p-b-15 p-t-20 dropdown-menu pop-profile">
                                 <div class="p-h-20 p-b-15 m-b-10 border-bottom">
-                                    <div class="d-flex m-r-50">
-                                        <div class="avatar avatar-lg avatar-image">
-                                            <img src="assets/images/avatars/thumb-3.jpg" alt="">
-                                        </div>
+                                    <div class="d-flex">
+                                    <div class="avatar avatar-text bg-primary">
+                                        <span>
+                                           <?php echo strtoupper(substr($this->session->userdata('username'), 0, 1)); //echo just the first letter of the username.?>
+                                        </span>
+                                    </div>
                                         <div class="m-l-10">
                                             <p class="m-b-0 text-dark font-weight-semibold">
                                                
